924cfc69d6d07723b742542ab5a47f9d72576529
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-27 18:37:32 +0000

    minor update
    git-svn-id: https://plugins.svn.wordpress.org/quick-login-link/trunk@1188675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,20 +3,13 @@
    Plugin Name: Quick Login link
    Description: Simply login not only through yoursite.com/wp-admin , but also: yoursite.com/login  (P.S.  OTHER MUST-HAVE PLUGINS FOR EVERYONE: http://bitly.com/MWPLUGINS  )
    contributors: selnomeria
-   Version: 1.2
+   Version: 1.21
    LICENCE: Free
 */
 if ( ! defined( 'ABSPATH' ) ) exit; //Exit if accessed directly
 
-$execut = new QuickLoginLinks;
-class QuickLoginLinks{
-	public $YOUR_CUSTOM_URL_ARRAY= array('/login','/admin','/l');
-	
-	public function __construct()			{	add_action('init', $this->start_test_detector() , 1); 	}
-	public function start_test_detector()	{ 	$homePath= home_url('','relative');
-		foreach ($this->YOUR_CUSTOM_URL_ARRAY as $each){
-			if (strtolower($_SERVER['REQUEST_URI']) == $homePath.$each){ echo '<script type="text/javascript">window.location = "'.admin_url('').'";</script>'; exit; }
-		}
-	}
+add_action('init', 'start_login_detector__QLL', 1);  function start_test_detector()	{	$h= home_url('','relative');
+
+	if (in_array(strtolower($_SERVER['REQUEST_URI']), array( $h.'/admin', $h.'/login',  $h.'/l' ))){ echo '<script type="text/javascript">window.location = "'.admin_url('').'";</script>'; exit; }
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -4,9 +4,9 @@ Tags: quick,fast,login,link,url,admin
 License: GPLv2
 Requires at least: 2.7
 Tested up to: 4.2.2
-Stable tag: 1.2
+Stable tag: 1.21
 
-Simply login not only through yoursite.com/wp-admin , but also: yoursite.com/login
+Simply login into Dashboard using (not only EXAMPLE.COM/wp-admin), but also EXAMPLE.COM/admin 
 
 == Description ==
 
